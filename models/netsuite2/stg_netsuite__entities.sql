051019fcb072cc74a8c24a91134954447c4a1363
Jamesyoub <jamesrayoub@gmail.com>
2022-04-01 11:39:52 -0400

    added entity model

---
+++ models/netsuite2/stg_netsuite__entities.sql
@@ -0,0 +1,49 @@
+{{ config(enabled=var('data_model') == 'netsuite2') }}
+
+with base as (
+
+    select * 
+    from {{ ref('stg_netsuite__entities_tmp') }}
+
+),
+
+fields as (
+
+    select
+        /*
+        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns 
+        that are expected/needed (staging_columns from dbt_salesforce_source/models/tmp/) and compares it with columns 
+        in the source (source_columns from dbt_salesforce_source/macros/).
+        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
+        */
+
+        {{
+            fivetran_utils.fill_staging_columns(
+                source_columns=adapter.get_columns_in_relation(ref('stg_netsuite__entities_tmp')),
+                staging_columns=get_netsuite2_entities_columns()
+            )
+        }}
+        
+    from base
+),
+
+final as (
+    
+    select
+        _fivetran_synced,
+        id as entity_id,
+        parent as parent_id,
+        entitytitle as entity_name,
+        type as entity_type,
+        isperson as is_person,
+        contact as contact_id,
+        customer as customer_id,
+        employee as employee_id,
+        project as job_id,
+        vendor as vendor_id
+        
+    from fields
+)
+
+select * 
+from final
