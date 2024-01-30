2376a3fa89a420853d3b6eaea640e425d5d932cd
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-01-30 16:01:01 -0400

    integration test variable scoping is weird, let's try adding it to CLI command

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -17,10 +17,10 @@ echo `pwd`
 cd integration_tests
 dbt deps
 dbt seed --target "$db" --full-refresh
-dbt run --target "$db" --full-refresh
+dbt run --target "$db" --vars '{integration_tests_seed_identifer_override: true}' --full-refresh
 dbt test --target "$db"
 ## UPDATE FOR VARS HERE, IF NO VARS, PLEASE REMOVE
-dbt run --vars '{netsuite2__multibook_accounting_enabled: false, netsuite2__using_exchange_rate: false, netsuite2__using_vendor_categories: false, netsuite2__using_jobs: false}' --target "$db" --full-refresh
+dbt run --vars '{integration_tests_seed_identifer_override: true, netsuite2__multibook_accounting_enabled: false, netsuite2__using_exchange_rate: false, netsuite2__using_vendor_categories: false, netsuite2__using_jobs: false}' --target "$db" --full-refresh
 dbt test --target "$db"
 ### END VARS CHUNK, REMOVE IF NOT USING
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
