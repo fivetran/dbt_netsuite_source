33cb56dba654ef8568c3016a2c171ccc25a198fc
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-04-08 19:58:28 -0500

    feature/performance-improvements

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -16,11 +16,22 @@ db=$1
 echo `pwd`
 cd integration_tests
 dbt deps
+
+if [ "$db" = "databricks-sql" ]; then
+dbt seed --vars '{netsuite_schema: netsuite_source_integrations_tests_sqlw}' --target "$db" --full-refresh
+dbt compile --vars '{netsuite_schema: netsuite_source_integrations_tests_sqlw}' --target "$db"
+dbt run --vars '{netsuite_schema: netsuite_source_integrations_tests_sqlw}' --target "$db" --full-refresh
+dbt test --vars '{netsuite_schema: netsuite_source_integrations_tests_sqlw}' --target "$db"
+dbt run --vars '{netsuite_schema: netsuite_source_integrations_tests_sqlw, netsuite2__multibook_accounting_enabled: false, netsuite2__using_exchange_rate: false, netsuite2__using_vendor_categories: false, netsuite2__using_jobs: false}' --target "$db" --full-refresh
+dbt test --target "$db"
+
+else
+
 dbt seed --target "$db" --full-refresh
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
-## UPDATE FOR VARS HERE, IF NO VARS, PLEASE REMOVE
 dbt run --vars '{netsuite2__multibook_accounting_enabled: false, netsuite2__using_exchange_rate: false, netsuite2__using_vendor_categories: false, netsuite2__using_jobs: false}' --target "$db" --full-refresh
 dbt test --target "$db"
-### END VARS CHUNK, REMOVE IF NOT USING
+fi
+
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,16 @@
+# dbt_netsuite_source v0.10.0
+[PR #47](https://github.com/fivetran/dbt_netsuite_source/pull/47) includes the following update:
+## 🚨 Breaking Changes 🚨
+- To reduce storage, updated default materialization of staging models to views. 
+>  ⚠️ Running a `--full-refresh` will be required if you have previously run these staging models as tables and get the following error: 
+> ```
+> Trying to create view <model path> but it currently exists as a table. Either drop <model path> manually, or run dbt with `--full-refresh` and dbt will drop it for you.
+> ```
+
+## Under the Hood:
+- Added integration testing pipeline for Databricks SQL Warehouse.
+- Included auto-releaser GitHub Actions workflow to automate future releases.
+
 # dbt_netsuite_source v0.9.0
 [PR #42](https://github.com/fivetran/dbt_netsuite_source/pull/42) includes the following update:
 ## 🚨 Breaking Changes 🚨:
